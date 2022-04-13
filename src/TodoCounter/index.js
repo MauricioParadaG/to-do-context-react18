03b158916475565a85ff72209fd1a5213e4678d4
Mauricio Parada <paradaj@uninorte.edu.co>
2022-04-13 10:07:20 +0200

    useContext instead of Context.Consumer ,dynamic renders

--- src/TodoCounter/index.js
+++ src/TodoCounter/index.js
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { TodoContext } from '../TodoContext';
 import './TodoCounter.css';
 
+function TodoCounter() {
+  const {totalTodos:total, completedTodos:completed } = useContext(TodoContext);
 
-function TodoCounter({ total, completed }) {
   return (
     <h2 className="TodoCounter">Has completado {completed} de {total} TODOs</h2>
   );
