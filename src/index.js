2eb2f006716b212a13a7ff33f3808a2a19860bcb
Mauricio Parada <paradaj@uninorte.edu.co>
2022-04-16 12:21:25 +0200

    Small example of High order components to be deleted

--- src/index.js
+++ src/index.js
@@ -1,14 +1,36 @@
 import React from 'react';
 import ReactDom from 'react-dom/client';
-import App from './App';
+/* import App from './App'; */
 import './index.css';
+
+function App(props) {
+  return (
+    <h1>{props.saludo}, from {props.name}</h1>
+  )
+}
+
+function withSaludoHoc(WrappedComponent) {
+  return function WrappedComponentWithSaludo (saludo) {
+    return function ComponenteDeVerdad(props) {
+      return (
+        <>
+          <WrappedComponent {...props} saludo={saludo}/>
+          <h2>Dentro del HOC abajo del WrappedComponent = App</h2>
+        </>
+      )
+    }
+  }
+}
+
+const AppWithSaludoHoc = withSaludoHoc(App)("El prop Saludo");
 
 const rootElement = document.getElementById('root');
 const root = ReactDom.createRoot(rootElement);
 
 root.render(
   <React.StrictMode>
-    <App />
+    <AppWithSaludoHoc name="Props"/>
+    {/* <App saludo="Hola" name="Props" /> */}
   </React.StrictMode>
 );
 
